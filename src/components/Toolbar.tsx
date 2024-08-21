d34acab4fbfcc54911e5cde877b319b0f7b4e498
Michael Little <michael.francis.little@gmail.com>
2024-08-21 21:04:44 +1200

    Very basic toolbar

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,13 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowPointer, faImage } from "@fortawesome/free-solid-svg-icons"
 
 import "./Toolbar.scss"
 
+type SelectedTool = 'select' | 'sprite';
+
 export const Toolbar = () => {
+    const [selectedTool, setSelectedTool] = useState<SelectedTool | undefined>();
+
     return <div className="toolbar">
         <h2 className="tools_title">Tools</h2>
         <ul className="tools">
-            <li className="tool">Select Tool</li>
-            <li className="tool">Stamp Tool</li>
+            <li className={`tool ${selectedTool === 'select' ? 'is-selected' : ''}`} onClick={() => setSelectedTool('select')}>
+                <span className="tool-name">Select Tool</span>
+                <FontAwesomeIcon icon={faArrowPointer} />
+            </li>
+            <li className={`tool ${selectedTool === 'sprite' ? 'is-selected' : ''}`}  onClick={() => setSelectedTool('sprite')}>
+                <span className="tool-name">Sprite Tool</span>
+                <FontAwesomeIcon icon={faImage} />
+            </li>
         </ul>
     </div>
 }
