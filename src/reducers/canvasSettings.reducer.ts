55b03f3980c876d2fcf064e00a9769d2fc3ecd66
Michael Little <michael.francis.little@gmail.com>
2025-02-24 02:00:09 +1300

    formatting

--- src/reducers/canvasSettings.reducer.ts
+++ src/reducers/canvasSettings.reducer.ts
@@ -1,18 +1,20 @@
-import { createReducer } from "@reduxjs/toolkit";
+import { createReducer } from '@reduxjs/toolkit';
 import * as actions from '~actions';
 
 interface IState {
-    aspectRatio: number;
-    backgroundAsset?: string;
+  aspectRatio: number;
+  backgroundAsset?: string;
 }
 
 const initialState: IState = {
-    aspectRatio: 1,
-    backgroundAsset: 'grass_tile'
-}
+  aspectRatio: 1,
+  backgroundAsset: 'grass_tile',
+};
 
-export const canvasSettingsReducer = createReducer(initialState, builder => builder.addCase(actions.setCanvasSettings, (state, {payload}) => ({
+export const canvasSettingsReducer = createReducer(initialState, (builder) =>
+  builder.addCase(actions.setCanvasSettings, (state, { payload }) => ({
     ...state,
     aspectRatio: payload.aspectRatio,
-    backgroundAsset: payload.backgroundAsset
-})))
+    backgroundAsset: payload.backgroundAsset,
+  })),
+);
